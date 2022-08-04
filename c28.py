7d341aff77760f8e291fcc63002f68d3bec7703e
Partha Pratim Datta <92267112+devops-parth@users.noreply.github.com>
2022-08-04 21:35:00 +0530

    Cereating error

--- c28.py
+++ c28.py
@@ -1,37 +1,43 @@
-# range() with one parameter
-foX val in range(6):
-    print(val, end='  ')
-print('\n')
+asdadad
+asdad
+asdadfa
+adfag
+gretth
 
-foX val in range(6):
-    print(val, end='  ')
-print('\n')
-
-foX val in range(6):
-    print(val, end='  ')
-print('\n')
-
-
-foX val in range(6):
-    print(val, end='  ')
-print('\n')
+asdadad
+asdad
+asdadfa
+adfag
+gretth
 
 
+asdadad
+asdad
+asdadfa
+adfag
+gretth
 
-foX val in range(6):
-    print(val, end='  ')
-print('\n')
 
 
-foX val in range(6):
-    print(val, end='  ')
-print('\n')
-
-# range() with two parameter
-foX val in range(5,10):
-    print(val, end='  ')
-print('\n')
-
-# range() with three parameter
-for val in range(0,8,2):
-    print(val, end='  ')
+asdadad
+asdad
+asdadfa
+adfag
+gretthasdadad
+asdad
+asdadfa
+adfag
+gretth
+asdadad
+asdad
+asdadfa
+adfag
+gretthasdadad
+asdad
+asdadfa
+adfag
+gretthasdadad
+asdad
+asdadfa
+adfag
+gretth
