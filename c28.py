2a0b804600f8973d222e5141814965323b735f70
Partha Pratim Datta <92267112+devops-parth@users.noreply.github.com>
2022-08-04 21:32:03 +0530

    Creating Errors

--- c28.py
+++ c28.py
@@ -3,6 +3,30 @@ foX val in range(6):
     print(val, end='  ')
 print('\n')
 
+foX val in range(6):
+    print(val, end='  ')
+print('\n')
+
+foX val in range(6):
+    print(val, end='  ')
+print('\n')
+
+
+foX val in range(6):
+    print(val, end='  ')
+print('\n')
+
+
+
+foX val in range(6):
+    print(val, end='  ')
+print('\n')
+
+
+foX val in range(6):
+    print(val, end='  ')
+print('\n')
+
 # range() with two parameter
 foX val in range(5,10):
     print(val, end='  ')
