080d8048f5a7ee41eb394f21283b5a7e8552d78c
Partha Pratim Datta <92267112+devops-parth@users.noreply.github.com>
2022-08-04 22:11:58 +0530

    Creating Bad Python Code

--- c28.py
+++ c28.py
@@ -1,17 +1,23 @@
-hgdhgdjgfhg
-gfhfhkjhg
-jhhffjhghj
-hhjghg
-jkkgkjgkj
+def func_x(num):
+    if num == 1:
+        return a()
+    elif num == 2:
+        return b()
+    elif num == 3:
+        return c()
+    elif num == 4:
+        return d()
+    elif num == 5:
+        return e()
 
 
-
-hjhghfkhf
-bhghgjhkj
-hjhfhfjhff
-hhjghjfgfg
-
-
-125433646568
-
-hfhljh
+# Better
+def func_y(num):
+    mapper = {
+        1: a,
+        2: b,
+        3: c,
+        4: d,
+        5: e
+    }
+    return mapper[num]()
